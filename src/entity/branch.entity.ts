8c5433aafaf81bf9e84326aca0a0935d94aacc00
Alexandr_Hryhorov <alexandrhryhorov@yojji.io>
2021-12-07 18:02:19 +0200

    install fake and class validator

--- src/entity/branch.entity.ts
+++ src/entity/branch.entity.ts
@@ -1,3 +1,4 @@
+import { Fixture } from 'class-fixtures-factory';
 import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
 import { IdNameColumns } from '../db/shared-columns';
 import { Employee } from './employee.entity';
@@ -6,15 +7,19 @@ import { Transaction } from './transaction.entity';
 @Entity()
 export class Branch extends IdNameColumns {
   @Column({ length: 30 })
+  @Fixture((faker) => faker.address.streetName())
   address: string;
 
   @Column({ length: 20 })
+  @Fixture((faker) => faker.address.city())
   city: string;
 
   @Column({ length: 2 })
+  @Fixture((faker) => faker.address.state(true))
   state: string;
 
   @Column({ length: 12 })
+  @Fixture((faker) => faker.address.zipCode())
   zip: string;
 
   @OneToMany(() => Employee, (employee) => employee.id)
